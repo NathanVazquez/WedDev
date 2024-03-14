96b11edc7e4978c9328a6dfa5070a682ac242991
NathanVazquez <nathan.vazquez58@myhunter.cuny.edu>
2024-03-13 20:12:24 -0400

    first JS file, testing helloworld.js

---
+++ Classwork/helloworld.js
@@ -0,0 +1,28 @@
+
+let introduction = "Hello World";
+console.log(introduction);
+
+for (let line = "#"; line.length < 8; line += "#")
+  console.log(line);
+
+  //review string methods
+  let height = prompt("give height");
+  let width = prompt("give width");
+let line1;
+let line2;
+  for( let i =0  ; i<width;i++){
+    if(i%2==0){
+        line2 += "#";
+        line1 += " ";
+    }else {
+        line2 += " ";
+        line1 += "#";
+    }
+  }
+  for (let i =0;i<height;i++){
+    if(i%2==0){
+        console.log(line1);
+    }else {
+        console.log(line2);
+    }
+  }
