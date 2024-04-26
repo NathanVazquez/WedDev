76c4a9a8d6bcee50e2557d1048cc426b440498a6
NathanVazquez <nathan.vazquez58@myhunter.cuny.edu>
2024-04-25 22:44:45 -0400

    email form feature is working, and front page is fixed for most screen sizes

--- Classwork/helloworld.js
+++ Classwork/helloworld.js
@@ -2,27 +2,40 @@
 let introduction = "Hello World";
 console.log(introduction);
 
-for (let line = "#"; line.length < 8; line += "#")
-  console.log(line);
+function pascalsTriangle() {
 
-  //review string methods
-  let height = prompt("give height");
-  let width = prompt("give width");
-let line1;
-let line2;
-  for( let i =0  ; i<width;i++){
-    if(i%2==0){
-        line2 += "#";
-        line1 += " ";
-    }else {
-        line2 += " ";
-        line1 += "#";
+  for (let line = "#"; line.length < 8; line += "#")
+    console.log(line);
+    let height = 6;
+    let width = 6;
+    //review string methods
+    //let height = prompt("give height");
+    //let width = prompt("give width");
+  let line1;
+  let line2;
+    for( let i =0  ; i<width;i++){
+      if(i%2==0){
+          line2 += "#";
+          line1 += " ";
+      }else {
+          line2 += " ";
+          line1 += "#";
+      }
     }
-  }
-  for (let i =0;i<height;i++){
-    if(i%2==0){
-        console.log(line1);
-    }else {
-        console.log(line2);
+    for (let i =0;i<height;i++){
+      if(i%2==0){
+          console.log(line1);
+      }else {
+          console.log(line2);
+      }
     }
-  }
+}
+
+function chessboard(){
+
+
+
+}
+
+
+
